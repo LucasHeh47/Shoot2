86bb80da59db5ae2a3629fde02e9ca5a8850feab
LucasHeh47 <darthmonkey062005@gmail.com>
2021-04-13 13:53:36 -0700

    Beginning of adding combat

---
+++ Assets/Scripts/Managers/GunManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager : MonoBehaviour
+{
+    public Color NoColor;
+    public Color CommonColor;
+    public Color UnCommonColor;
+    public Color RareColor;
+    public Color EpicColor;
+    public Color LegendaryColor;
+
+    public Gun Pistol;
+    public Gun AK47;
+    public Gun Sniper;
+    public Gun UZI;
+    public Gun Remington;
+    public Gun M16;
+
+    public Gun[] AllGuns;
+
+    public static GunManager Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Gun GetGunByBulletImage(Sprite sprite)
+    {
+        for (int i = 0; i < AllGuns.Length; i++)
+        {
+            if (AllGuns[i].ProjectileImage == sprite)
+            {
+                return AllGuns[i];
+            }
+        }
+        Debug.Log("Null GetGunByBulletImage method at GunManager.cs");
+        return null;
+    }
+
+    public enum RarityTypes { Common, UnCommon, Rare, Epic, Legendary };
+
+    public Color GetColor(RarityTypes rarity)
+    {
+        if (rarity == RarityTypes.Common)
+        {
+            return CommonColor;
+        }
+        else if (rarity == RarityTypes.UnCommon)
+        {
+            return UnCommonColor;
+        }
+        else if (rarity == RarityTypes.Rare)
+        {
+            return RareColor;
+        }
+        else if (rarity == RarityTypes.Epic)
+        {
+            return EpicColor;
+        }
+        else if (rarity == RarityTypes.Legendary)
+        {
+            return LegendaryColor;
+        }
+        return NoColor;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Gun.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
+public class Gun : ScriptableObject
+{
+
+    public float Price;
+    public AudioClip ShootSound;
+    public Sprite GunImage;
+    public Sprite ProjectileImage;
+    public string GunName;
+    public bool DefaultGun = false;
+    public float BulletSpeed;
+    public float ReloadTime;
+    public int ClipSize;
+    public int AmmoInClip;
+    public int Ammo;
+    public int CurrentAmmo;
+    public bool Pierces;
+
+    public GunManager.RarityTypes Rarity;
+
+    // COMMON
+    public int CommonCritChance;
+    public float CommonDamage;
+    public float CommonFireRate;
+    public float CommonRange;
+
+    // UNCOMMON
+    public int UnCommonCritChance;
+    public float UnCommonDamage;
+    public float UnCommonFireRate;
+    public float UnCommonRange;
+
+    // RARE
+    public int RareCritChance;
+    public float RareDamage;
+    public float RareFireRate;
+    public float RareRange;
+
+    // EPIC
+    public int EpicCritChance;
+    public float EpicDamage;
+    public float EpicFireRate;
+    public float EpicRange;
+
+    // LEGENDARY
+    public int LegendaryCritChance;
+    public float LegendaryDamage;
+    public float LegendaryFireRate;
+    public float LegendaryRange;
+
+    public float CommonToUnCommonCost;
+    public float UnCommonToRareCost;
+    public float RareToEpicCost;
+    public float EpicToLegendaryCost;
+}
